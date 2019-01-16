17e812b828fbd4f8e156a1eeee6a6e78748c30ab
James Eagle <jimeagle@gmail.com>
2019-01-16 21:50:33 +0000

    Convert to float

--- unmarshal/unmarshal.go
+++ unmarshal/unmarshal.go
@@ -67,12 +67,55 @@ func fix(source map[string]interface{}, destinationType reflect.Type) (ret map[s
 			source[fieldName] = toBool(srcValKind, destinationFieldKind, sourceVal, fieldName)
 			break
 
+		case reflect.Float64:
+
+			source[fieldName] = toFloat(srcValKind, destinationFieldKind, sourceVal, fieldName)
+			break
+
 		default:
 			errLog(srcValKind, destinationFieldKind, fieldName)
 		}
 	}
 
 	return source
+}
+
+func toFloat(srcValKind reflect.Kind, destinationFieldKind reflect.Kind, srcVal interface{}, fieldName string) float64 {
+
+	switch srcValKind {
+
+	case reflect.String:
+
+		f, err := strconv.ParseFloat(srcVal.(string), 64)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return f
+
+	case reflect.Int:
+
+		return float64(srcVal.(int))
+
+	case reflect.Int64:
+
+		return float64(srcVal.(int64))
+
+	case reflect.Float64:
+
+		return srcVal.(float64)
+
+	case reflect.Bool:
+
+		if srcVal.(bool) {
+			return 1
+		}
+		return 0
+
+	default:
+
+		errLog(srcValKind, destinationFieldKind, fieldName)
+		return 0
+	}
 }
 
 func toBool(srcValKind reflect.Kind, destinationFieldKind reflect.Kind, srcVal interface{}, fieldName string) interface{} {
