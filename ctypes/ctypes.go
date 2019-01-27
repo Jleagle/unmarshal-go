c1a1e5e52a9102791691101754d6c59e30f9c748
James Eagle <jimeagle@gmail.com>
2019-01-27 12:02:54 +0000

    Add CInt64 type

--- ctypes/ctypes.go
+++ ctypes/ctypes.go
@@ -127,6 +127,71 @@ func (i *CInt) UnmarshalJSON(b []byte) error {
 	return errors.New("can not convert " + types[dataType] + " to int")
 }
 
+type CInt64 int64
+
+func (i *CInt64) UnmarshalJSON(b []byte) error {
+
+	var data, dataType, _, err = jsonparser.Get(b)
+	if err != nil {
+		return err
+	}
+
+	if len(data) == 0 {
+		*i = 0
+		return nil
+	}
+
+	str := string(data)
+
+	switch dataType {
+	case jsonparser.String, jsonparser.Number:
+
+		if strings.Contains(str, ".") {
+
+			j, err := strconv.ParseFloat(str, 64)
+			if err != nil {
+				return err
+			}
+			*i = CInt64(j)
+
+		} else {
+
+			k, err := strconv.ParseInt(str, 10, 64)
+			if err != nil {
+				return err
+			}
+			*i = CInt64(k)
+
+		}
+
+		return nil
+
+	case jsonparser.Boolean:
+
+		b, err := strconv.ParseBool(str)
+		if err != nil {
+			return err
+		}
+
+		if b {
+			*i = 1
+		} else {
+			*i = 0
+		}
+
+		return nil
+
+	case jsonparser.Null:
+
+		*i = 0
+
+		return nil
+
+	}
+
+	return errors.New("can not convert " + types[dataType] + " to int64")
+}
+
 type CBool bool
 
 func (i *CBool) UnmarshalJSON(b []byte) error {
